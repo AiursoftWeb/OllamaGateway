974a73e87c03e4efa45a95d66dee682dc5a77907
Anduin Xue <anduin@aiursoft.com>
2026-03-18 13:52:57 +0000

    feat: Add Embedding Lab page for generating embeddings with selected models

--- src/Aiursoft.OllamaGateway/Controllers/ChatPlaygroundController.cs
+++ src/Aiursoft.OllamaGateway/Controllers/ChatPlaygroundController.cs
@@ -49,4 +49,41 @@ public class ChatPlaygroundController(
         ViewData["AllModels"] = models;
         return this.StackView(viewModel);
     }
+
+    [RenderInNavBar(
+        NavGroupName = "Chat",
+        NavGroupOrder = 100,
+        CascadedLinksGroupName = "Playground",
+        CascadedLinksIcon = "message-square",
+        CascadedLinksOrder = 1,
+        LinkText = "Embedding Lab",
+        LinkOrder = 2)]
+    [HttpGet]
+    public async Task<IActionResult> EmbeddingLab(int? id)
+    {
+        var models = await dbContext.VirtualModels
+            .Where(m => m.Type == ModelType.Embedding)
+            .OrderBy(m => m.Name)
+            .ToListAsync();
+
+        if (!models.Any())
+        {
+            return RedirectToAction("Index", "VirtualModels");
+        }
+
+        var selectedModel = id.HasValue 
+            ? models.FirstOrDefault(m => m.Id == id.Value) ?? models.First()
+            : models.First();
+
+        var viewModel = new CreateViewModel // Reuse this for model info
+        {
+            Name = selectedModel.Name,
+            UnderlyingModel = selectedModel.UnderlyingModel,
+            PageTitle = "Embedding Lab"
+        };
+        
+        ViewData["ModelId"] = selectedModel.Id;
+        ViewData["AllModels"] = models;
+        return this.StackView(viewModel);
+    }
 }
